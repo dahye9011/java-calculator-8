5f81b48cef3da4776de5fa55bcb0f2920a8f0985
dahye <dahye9011@naver.com>
2025-10-17 13:39:15 +0900

    test(Calculator): 테스트 케이스 작성

---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,72 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CalculatorTest {
+    @Test
+    void 기본_구분자_사용1() {
+        // given
+        Calculator calculator = new Calculator();
+        String input = "1,2,3";
+
+        // when
+        int result = calculator.calculate(input);
+
+        // then
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    void 기본_구분자_사용2() {
+        // given
+        Calculator calculator = new Calculator();
+        String input = "1:2:3";
+
+        // when
+        int result = calculator.calculate(input);
+
+        // then
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    void 기본_구분자_혼합_사용() {
+        // given
+        Calculator calculator = new Calculator();
+        String input = "1,2:3";
+
+        // when
+        int result = calculator.calculate(input);
+
+        // then
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    void 빈_문자열() {
+        // given
+        Calculator calculator = new Calculator();
+        String input = "";
+
+        // when
+        int result = calculator.calculate(input);
+
+        // then
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    void 구분자_없는_문자열() {
+        // given
+        Calculator calculator = new Calculator();
+        String input = "123";
+
+        // when
+        int result = calculator.calculate(input);
+
+        // then
+        assertThat(result).isEqualTo(123);
+    }
+}
