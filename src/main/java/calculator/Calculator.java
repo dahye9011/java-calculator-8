939dd2b61b11ee73f35ea88c47db3b633caf80d1
dahye <dahye9011@naver.com>
2025-10-18 15:52:42 +0900

    feat(Calculator): 기본 구분자 문자열 덧셈 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,4 +1,20 @@
 package calculator;
 
+import java.util.Arrays;
+
 public class Calculator {
+
+    public int calculate(String input) {
+        if (input == null || input.isBlank()) {
+            return 0;
+        }
+
+        String[] result = input.replace(" ", "").split("[,:]");
+
+        int sum = Arrays.stream(result)
+                .mapToInt(Integer::parseInt)
+                .sum();
+
+        return sum;
+    }
 }
