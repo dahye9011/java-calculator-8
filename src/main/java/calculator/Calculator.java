e1ee43558c46bd1c58a062800a2781b5a798bbdf
dahye <dahye9011@naver.com>
2025-10-18 16:59:09 +0900

    feat(Calculator): 커스텀 구분자 문자열 덧셈 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -9,9 +9,35 @@ public class Calculator {
             return 0;
         }
 
-        String[] result = input.replace(" ", "").split("[,:]");
+        if (input.startsWith("//")) {
+            // \n 뒤부터 잘라서 일반 입력 문자열로 만듦
+            // 입력 문자열을 커스텀 구분자로 자름
+            int newLineIndex = input.indexOf("\n");
 
-        int sum = Arrays.stream(result)
+            String inputString = input.substring(newLineIndex + 1);
+
+            String customDelimiter = separateDelimiter(input);
+            String[] splitString = inputString.replace(" ", "").split(customDelimiter);
+
+            int result = sum(splitString);
+            return result;
+        }
+
+        String[] splitString = input.replace(" ", "").split("[,:]");
+        int result = sum(splitString);
+
+        return result;
+    }
+
+    private String separateDelimiter(String input) {
+        int newLineIndex = input.indexOf("\n");
+        String customDelimiter = input.substring(2, newLineIndex);
+
+        return customDelimiter;
+    }
+
+    private int sum(String[] input) {
+        int sum = Arrays.stream(input)
                 .mapToInt(Integer::parseInt)
                 .sum();
 
