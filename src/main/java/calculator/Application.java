345b58930058d2c76788e3611c8c0b011b151956
dahye <dahye9011@naver.com>
2025-10-19 22:06:12 +0900

    feat(Application): main 메서드 실행 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,17 @@
 package calculator;
 
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        Calculator calculator = new Calculator();
+
+        String input = inputView.readInput();
+        int result = calculator.calculate(input);
+        outputView.printResult(result);
     }
 }
