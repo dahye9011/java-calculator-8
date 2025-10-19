7cb94eb08a4fdcaea5c68a6681c4e03b864d167c
dahye <dahye9011@naver.com>
2025-10-19 23:04:52 +0900

    refactor(Parser): 입력 추출 및 숫자 변환 분리

--- src/main/java/calculator/Parser.java
+++ src/main/java/calculator/Parser.java
@@ -1,14 +1,17 @@
 package calculator;
 
-public class DelimiterParser {
+public class Parser {
     public static String extractCustomDelimiter(String input) {
-        int newLineIndex = input.indexOf("\n");
-
-        Validator.validateCustomHeader(newLineIndex);
+        int newLineIndex = getNewLineIndex(input);
         String delimiter = input.substring(2, newLineIndex);
         Validator.validateDelimiterFormat(delimiter);
 
         return delimiter;
+    }
+
+    public static String extractPayload(String input) {
+        int newLineIndex = getNewLineIndex(input);
+        return input.substring(newLineIndex + 1);
     }
 
     public static int parseNumber(String token) {
@@ -17,5 +20,11 @@ public class DelimiterParser {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("숫자 외의 문자가 포함되어 있습니다.");
         }
+    }
+
+    private static int getNewLineIndex(String input) {
+        int newLineIndex = input.indexOf("\n");
+        Validator.validateCustomHeader(newLineIndex);
+        return newLineIndex;
     }
 }
